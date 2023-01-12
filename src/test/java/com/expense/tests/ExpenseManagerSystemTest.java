e95ca8d628605188f702de0616ad2f4b779050c9
umangagarwalit <umangagarwalit@gmail.com>
2023-01-13 00:45:03 +0530

    CodeCommit

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -37,10 +37,10 @@ public class ExpenseManagerSystemTest {
 		
 		
 		//Buggy Code
-		String expectedTitle = "Hello Page";
+		//String expectedTitle = "Hello Page";
 		
 		//Correct Code
-		//String expectedTitle = "Login page";
+		String expectedTitle = "Login page";
 		
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(expectedTitle, actualTitle);
@@ -52,6 +52,7 @@ public class ExpenseManagerSystemTest {
 		
 		driver.quit();
 		
+		/*
         if(result.getStatus() == ITestResult.FAILURE) {
 			//You need to enter your JIRA Username and Password in below line
         	BasicCredentials cred = new BasicCredentials("", "");
@@ -63,7 +64,7 @@ public class ExpenseManagerSystemTest {
         	Issue issueName = jira.createIssue("", "Bug").field(Field.SUMMARY, result.getMethod().getMethodName() +"is failed due to: "+ result.getThrowable().toString()).field(Field.DESCRIPTION, "get the description").execute();
         	System.out.println("Issue is created in Jira with Issue Key: "+issueName.getKey());
         }
-
+		*/
 	}
 	
 	

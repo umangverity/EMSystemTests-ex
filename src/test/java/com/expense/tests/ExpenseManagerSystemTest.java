bee631773d19adb00e25f21457d7aaf88dfea96e
umangagarwalit <umangagarwalit@gmail.com>
2023-01-20 00:14:27 +0530

    CodeCommit

--- src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
+++ src/test/java/com/expense/tests/ExpenseManagerSystemTest.java
@@ -55,10 +55,10 @@ public class ExpenseManagerSystemTest {
 		
         if(result.getStatus() == ITestResult.FAILURE) {
 			//You need to enter your JIRA Username and Password in below line
-        	BasicCredentials cred = new BasicCredentials("UmangVerity", "Saltuniv@123");
+        	BasicCredentials cred = new BasicCredentials("umangverity", "jirapassword");
 			
 			//You need to enter your JIRA machine IP address with port 8080 in below line
-        	JiraClient jira = new JiraClient("https://umangverity1.atlassian.net/jira/dashboards/10000", cred);
+        	JiraClient jira = new JiraClient("http://54.91.92.180:8080", cred);
 			
 			//You need to enter your JIRA project key in below line
         	Issue issueName = jira.createIssue("AUT", "Bug").field(Field.SUMMARY, result.getMethod().getMethodName() +"is failed due to: "+ result.getThrowable().toString()).field(Field.DESCRIPTION, "get the description").execute();
